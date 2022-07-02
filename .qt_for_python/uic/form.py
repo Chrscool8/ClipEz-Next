0fc11f91515f6d129d55c9e021a6f125dcd125a0
Chris Bradel <ChrisBradel14@gmail.com>
2022-07-02 00:33:13 -0400

    Thumbnail grabbing and preview

--- .qt_for_python/uic/form.py
+++ .qt_for_python/uic/form.py
@@ -15,145 +15,211 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QButtonGroup, QDoubleSpinBox, QFormLayout,
-    QGraphicsView, QGridLayout, QGroupBox, QLabel,
-    QListView, QListWidget, QListWidgetItem, QRadioButton,
-    QSizePolicy, QSpacerItem, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QDial, QFrame, QGridLayout,
+    QGroupBox, QHBoxLayout, QHeaderView, QLabel,
+    QLineEdit, QProgressBar, QPushButton, QSizePolicy,
+    QSpacerItem, QTabWidget, QTableWidget, QTableWidgetItem,
+    QTextEdit, QTreeView, QVBoxLayout, QWidget)
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        if not Form.objectName():
-            Form.setObjectName(u"Form")
-        Form.resize(545, 471)
-        self.gridLayout = QGridLayout(Form)
-        self.gridLayout.setObjectName(u"gridLayout")
-        self.easingCurvePicker = QListWidget(Form)
-        self.easingCurvePicker.setObjectName(u"easingCurvePicker")
-        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+class Ui_Widget(object):
+    def setupUi(self, Widget):
+        if not Widget.objectName():
+            Widget.setObjectName(u"Widget")
+        Widget.resize(1165, 805)
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.easingCurvePicker.sizePolicy().hasHeightForWidth())
-        self.easingCurvePicker.setSizePolicy(sizePolicy)
-        self.easingCurvePicker.setMaximumSize(QSize(16777215, 120))
-        self.easingCurvePicker.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.easingCurvePicker.setMovement(QListView.Static)
-        self.easingCurvePicker.setProperty("isWrapping", False)
-        self.easingCurvePicker.setViewMode(QListView.IconMode)
-        self.easingCurvePicker.setSelectionRectVisible(False)
-
-        self.gridLayout.addWidget(self.easingCurvePicker, 0, 0, 1, 2)
-
-        self.verticalLayout = QVBoxLayout()
+        sizePolicy.setHeightForWidth(Widget.sizePolicy().hasHeightForWidth())
+        Widget.setSizePolicy(sizePolicy)
+        self.verticalLayout_4 = QVBoxLayout(Widget)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.tabs_overall = QTabWidget(Widget)
+        self.tabs_overall.setObjectName(u"tabs_overall")
+        self.tab_downloadvideo = QWidget()
+        self.tab_downloadvideo.setObjectName(u"tab_downloadvideo")
+        self.verticalLayout_3 = QVBoxLayout(self.tab_downloadvideo)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.groupbox_videourl = QGroupBox(self.tab_downloadvideo)
+        self.groupbox_videourl.setObjectName(u"groupbox_videourl")
+        self.verticalLayout = QVBoxLayout(self.groupbox_videourl)
         self.verticalLayout.setObjectName(u"verticalLayout")
-        self.groupBox_2 = QGroupBox(Form)
-        self.groupBox_2.setObjectName(u"groupBox_2")
-        self.verticalLayout_2 = QVBoxLayout(self.groupBox_2)
-        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
-        self.lineRadio = QRadioButton(self.groupBox_2)
-        self.buttonGroup = QButtonGroup(Form)
-        self.buttonGroup.setObjectName(u"buttonGroup")
-        self.buttonGroup.setExclusive(True)
-        self.buttonGroup.addButton(self.lineRadio)
-        self.lineRadio.setObjectName(u"lineRadio")
-        self.lineRadio.setChecked(True)
-
-        self.verticalLayout_2.addWidget(self.lineRadio)
-
-        self.circleRadio = QRadioButton(self.groupBox_2)
-        self.buttonGroup.addButton(self.circleRadio)
-        self.circleRadio.setObjectName(u"circleRadio")
-
-        self.verticalLayout_2.addWidget(self.circleRadio)
-
-
-        self.verticalLayout.addWidget(self.groupBox_2)
-
-        self.groupBox = QGroupBox(Form)
-        self.groupBox.setObjectName(u"groupBox")
-        sizePolicy1 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Preferred)
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.lineedit_url = QLineEdit(self.groupbox_videourl)
+        self.lineedit_url.setObjectName(u"lineedit_url")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
         sizePolicy1.setHorizontalStretch(0)
         sizePolicy1.setVerticalStretch(0)
-        sizePolicy1.setHeightForWidth(self.groupBox.sizePolicy().hasHeightForWidth())
-        self.groupBox.setSizePolicy(sizePolicy1)
-        self.formLayout = QFormLayout(self.groupBox)
-        self.formLayout.setObjectName(u"formLayout")
-        self.formLayout.setFieldGrowthPolicy(QFormLayout.AllNonFixedFieldsGrow)
-        self.label = QLabel(self.groupBox)
-        self.label.setObjectName(u"label")
+        sizePolicy1.setHeightForWidth(self.lineedit_url.sizePolicy().hasHeightForWidth())
+        self.lineedit_url.setSizePolicy(sizePolicy1)
 
-        self.formLayout.setWidget(0, QFormLayout.LabelRole, self.label)
+        self.horizontalLayout.addWidget(self.lineedit_url)
 
-        self.periodSpinBox = QDoubleSpinBox(self.groupBox)
-        self.periodSpinBox.setObjectName(u"periodSpinBox")
-        self.periodSpinBox.setEnabled(False)
-        self.periodSpinBox.setMinimum(-1.000000000000000)
-        self.periodSpinBox.setSingleStep(0.100000000000000)
-        self.periodSpinBox.setValue(-1.000000000000000)
-
-        self.formLayout.setWidget(0, QFormLayout.FieldRole, self.periodSpinBox)
-
-        self.label_2 = QLabel(self.groupBox)
-        self.label_2.setObjectName(u"label_2")
-
-        self.formLayout.setWidget(1, QFormLayout.LabelRole, self.label_2)
-
-        self.amplitudeSpinBox = QDoubleSpinBox(self.groupBox)
-        self.amplitudeSpinBox.setObjectName(u"amplitudeSpinBox")
-        self.amplitudeSpinBox.setEnabled(False)
-        self.amplitudeSpinBox.setMinimum(-1.000000000000000)
-        self.amplitudeSpinBox.setSingleStep(0.100000000000000)
-        self.amplitudeSpinBox.setValue(-1.000000000000000)
-
-        self.formLayout.setWidget(1, QFormLayout.FieldRole, self.amplitudeSpinBox)
-
-        self.label_3 = QLabel(self.groupBox)
-        self.label_3.setObjectName(u"label_3")
-
-        self.formLayout.setWidget(2, QFormLayout.LabelRole, self.label_3)
-
-        self.overshootSpinBox = QDoubleSpinBox(self.groupBox)
-        self.overshootSpinBox.setObjectName(u"overshootSpinBox")
-        self.overshootSpinBox.setEnabled(False)
-        self.overshootSpinBox.setMinimum(-1.000000000000000)
-        self.overshootSpinBox.setSingleStep(0.100000000000000)
-        self.overshootSpinBox.setValue(-1.000000000000000)
-
-        self.formLayout.setWidget(2, QFormLayout.FieldRole, self.overshootSpinBox)
-
-
-        self.verticalLayout.addWidget(self.groupBox)
-
-        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.verticalLayout.addItem(self.verticalSpacer)
-
-
-        self.gridLayout.addLayout(self.verticalLayout, 1, 0, 1, 1)
-
-        self.graphicsView = QGraphicsView(Form)
-        self.graphicsView.setObjectName(u"graphicsView")
-        sizePolicy2 = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.button_cleartext = QPushButton(self.groupbox_videourl)
+        self.button_cleartext.setObjectName(u"button_cleartext")
+        self.button_cleartext.setEnabled(False)
+        sizePolicy2 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
         sizePolicy2.setHorizontalStretch(0)
         sizePolicy2.setVerticalStretch(0)
-        sizePolicy2.setHeightForWidth(self.graphicsView.sizePolicy().hasHeightForWidth())
-        self.graphicsView.setSizePolicy(sizePolicy2)
+        sizePolicy2.setHeightForWidth(self.button_cleartext.sizePolicy().hasHeightForWidth())
+        self.button_cleartext.setSizePolicy(sizePolicy2)
+        self.button_cleartext.setLayoutDirection(Qt.LeftToRight)
 
-        self.gridLayout.addWidget(self.graphicsView, 1, 1, 1, 1)
+        self.horizontalLayout.addWidget(self.button_cleartext)
 
 
-        self.retranslateUi(Form)
+        self.verticalLayout.addLayout(self.horizontalLayout)
 
-        QMetaObject.connectSlotsByName(Form)
+        self.horizontalLayout_3 = QHBoxLayout()
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.button_getinfo = QPushButton(self.groupbox_videourl)
+        self.button_getinfo.setObjectName(u"button_getinfo")
+        sizePolicy2.setHeightForWidth(self.button_getinfo.sizePolicy().hasHeightForWidth())
+        self.button_getinfo.setSizePolicy(sizePolicy2)
+
+        self.horizontalLayout_3.addWidget(self.button_getinfo)
+
+        self.button_downloadstart = QPushButton(self.groupbox_videourl)
+        self.button_downloadstart.setObjectName(u"button_downloadstart")
+        sizePolicy2.setHeightForWidth(self.button_downloadstart.sizePolicy().hasHeightForWidth())
+        self.button_downloadstart.setSizePolicy(sizePolicy2)
+
+        self.horizontalLayout_3.addWidget(self.button_downloadstart)
+
+        self.horizontalSpacer = QSpacerItem(40, 20, QSizePolicy.Expanding, QSizePolicy.Minimum)
+
+        self.horizontalLayout_3.addItem(self.horizontalSpacer)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_3)
+
+
+        self.verticalLayout_3.addWidget(self.groupbox_videourl)
+
+        self.groupbox_videoinfo = QGroupBox(self.tab_downloadvideo)
+        self.groupbox_videoinfo.setObjectName(u"groupbox_videoinfo")
+        self.horizontalLayout_2 = QHBoxLayout(self.groupbox_videoinfo)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.tabs_videoinfo = QTabWidget(self.groupbox_videoinfo)
+        self.tabs_videoinfo.setObjectName(u"tabs_videoinfo")
+        self.tab_videoinfo_simple = QWidget()
+        self.tab_videoinfo_simple.setObjectName(u"tab_videoinfo_simple")
+        self.gridLayout_4 = QGridLayout(self.tab_videoinfo_simple)
+        self.gridLayout_4.setObjectName(u"gridLayout_4")
+        self.table_simple = QTableWidget(self.tab_videoinfo_simple)
+        if (self.table_simple.columnCount() < 2):
+            self.table_simple.setColumnCount(2)
+        __qtablewidgetitem = QTableWidgetItem()
+        __qtablewidgetitem.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
+        self.table_simple.setHorizontalHeaderItem(0, __qtablewidgetitem)
+        __qtablewidgetitem1 = QTableWidgetItem()
+        __qtablewidgetitem1.setTextAlignment(Qt.AlignLeading|Qt.AlignVCenter);
+        self.table_simple.setHorizontalHeaderItem(1, __qtablewidgetitem1)
+        self.table_simple.setObjectName(u"table_simple")
+        sizePolicy3 = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy3.setHorizontalStretch(0)
+        sizePolicy3.setVerticalStretch(0)
+        sizePolicy3.setHeightForWidth(self.table_simple.sizePolicy().hasHeightForWidth())
+        self.table_simple.setSizePolicy(sizePolicy3)
+        self.table_simple.setAlternatingRowColors(True)
+        self.table_simple.setTextElideMode(Qt.ElideRight)
+        self.table_simple.horizontalHeader().setStretchLastSection(True)
+
+        self.gridLayout_4.addWidget(self.table_simple, 0, 0, 1, 1)
+
+        self.tabs_videoinfo.addTab(self.tab_videoinfo_simple, "")
+        self.tab_videoinfo_detailed = QWidget()
+        self.tab_videoinfo_detailed.setObjectName(u"tab_videoinfo_detailed")
+        self.gridLayout_2 = QGridLayout(self.tab_videoinfo_detailed)
+        self.gridLayout_2.setObjectName(u"gridLayout_2")
+        self.treeview_detailed = QTreeView(self.tab_videoinfo_detailed)
+        self.treeview_detailed.setObjectName(u"treeview_detailed")
+
+        self.gridLayout_2.addWidget(self.treeview_detailed, 0, 0, 1, 1)
+
+        self.tabs_videoinfo.addTab(self.tab_videoinfo_detailed, "")
+
+        self.horizontalLayout_2.addWidget(self.tabs_videoinfo)
+
+        self.image_thumb = QLabel(self.groupbox_videoinfo)
+        self.image_thumb.setObjectName(u"image_thumb")
+        self.image_thumb.setMinimumSize(QSize(160, 90))
+        self.image_thumb.setMaximumSize(QSize(160, 90))
+        self.image_thumb.setPixmap(QPixmap(u"G:/Documents/GitHub/Download-and-Clip/assets/16x9.png"))
+        self.image_thumb.setScaledContents(True)
+
+        self.horizontalLayout_2.addWidget(self.image_thumb)
+
+
+        self.verticalLayout_3.addWidget(self.groupbox_videoinfo)
+
+        self.groupbox_status = QGroupBox(self.tab_downloadvideo)
+        self.groupbox_status.setObjectName(u"groupbox_status")
+        self.verticalLayout_2 = QVBoxLayout(self.groupbox_status)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.textbox_status = QTextEdit(self.groupbox_status)
+        self.textbox_status.setObjectName(u"textbox_status")
+        self.textbox_status.setEnabled(True)
+        sizePolicy.setHeightForWidth(self.textbox_status.sizePolicy().hasHeightForWidth())
+        self.textbox_status.setSizePolicy(sizePolicy)
+        self.textbox_status.setMaximumSize(QSize(16777215, 16777215))
+        self.textbox_status.setFrameShadow(QFrame.Sunken)
+        self.textbox_status.setLineWrapMode(QTextEdit.NoWrap)
+        self.textbox_status.setReadOnly(True)
+
+        self.verticalLayout_2.addWidget(self.textbox_status)
+
+        self.progressbar_download = QProgressBar(self.groupbox_status)
+        self.progressbar_download.setObjectName(u"progressbar_download")
+        self.progressbar_download.setValue(0)
+        self.progressbar_download.setInvertedAppearance(False)
+
+        self.verticalLayout_2.addWidget(self.progressbar_download)
+
+
+        self.verticalLayout_3.addWidget(self.groupbox_status)
+
+        self.tabs_overall.addTab(self.tab_downloadvideo, "")
+        self.tab_syncpanel = QWidget()
+        self.tab_syncpanel.setObjectName(u"tab_syncpanel")
+        self.gridLayout_5 = QGridLayout(self.tab_syncpanel)
+        self.gridLayout_5.setObjectName(u"gridLayout_5")
+        self.dial = QDial(self.tab_syncpanel)
+        self.dial.setObjectName(u"dial")
+
+        self.gridLayout_5.addWidget(self.dial, 0, 0, 1, 1)
+
+        self.tabs_overall.addTab(self.tab_syncpanel, "")
+
+        self.verticalLayout_4.addWidget(self.tabs_overall)
+
+
+        self.retranslateUi(Widget)
+
+        self.tabs_overall.setCurrentIndex(0)
+        self.tabs_videoinfo.setCurrentIndex(0)
+
+
+        QMetaObject.connectSlotsByName(Widget)
     # setupUi
 
-    def retranslateUi(self, Form):
-        Form.setWindowTitle(QCoreApplication.translate("Form", u"Easing curves", None))
-        self.groupBox_2.setTitle(QCoreApplication.translate("Form", u"Path type", None))
-        self.lineRadio.setText(QCoreApplication.translate("Form", u"Line", None))
-        self.circleRadio.setText(QCoreApplication.translate("Form", u"Circle", None))
-        self.groupBox.setTitle(QCoreApplication.translate("Form", u"Properties", None))
-        self.label.setText(QCoreApplication.translate("Form", u"Period", None))
-        self.label_2.setText(QCoreApplication.translate("Form", u"Amplitude", None))
-        self.label_3.setText(QCoreApplication.translate("Form", u"Overshoot", None))
+    def retranslateUi(self, Widget):
+        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"ClipEz-Next", None))
+        self.groupbox_videourl.setTitle(QCoreApplication.translate("Widget", u"Video URL", None))
+        self.lineedit_url.setText(QCoreApplication.translate("Widget", u"https://youtu.be/MBNvk9CA1j4", None))
+        self.button_cleartext.setText(QCoreApplication.translate("Widget", u"Clear Text", None))
+        self.button_getinfo.setText(QCoreApplication.translate("Widget", u"Get Info", None))
+        self.button_downloadstart.setText(QCoreApplication.translate("Widget", u"Download", None))
+        self.groupbox_videoinfo.setTitle(QCoreApplication.translate("Widget", u"Video Info", None))
+        ___qtablewidgetitem = self.table_simple.horizontalHeaderItem(0)
+        ___qtablewidgetitem.setText(QCoreApplication.translate("Widget", u"key", None));
+        ___qtablewidgetitem1 = self.table_simple.horizontalHeaderItem(1)
+        ___qtablewidgetitem1.setText(QCoreApplication.translate("Widget", u"value", None));
+        self.tabs_videoinfo.setTabText(self.tabs_videoinfo.indexOf(self.tab_videoinfo_simple), QCoreApplication.translate("Widget", u"Simple", None))
+        self.tabs_videoinfo.setTabText(self.tabs_videoinfo.indexOf(self.tab_videoinfo_detailed), QCoreApplication.translate("Widget", u"Detailed", None))
+        self.image_thumb.setText("")
+        self.groupbox_status.setTitle(QCoreApplication.translate("Widget", u"Status", None))
+        self.tabs_overall.setTabText(self.tabs_overall.indexOf(self.tab_downloadvideo), QCoreApplication.translate("Widget", u"Download Video", None))
+        self.tabs_overall.setTabText(self.tabs_overall.indexOf(self.tab_syncpanel), QCoreApplication.translate("Widget", u"Sync Channels", None))
     # retranslateUi
 
--- widget.py
+++ widget.py
@@ -7,10 +7,12 @@ from pathlib import Path
 import yt_dlp
 from PySide6.QtCore import QFile
 from PySide6.QtUiTools import QUiLoader
-from PySide6.QtWidgets import QApplication, QPushButton, QWidget
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
 from PySide6.examples.widgets.itemviews.jsonmodel import jsonmodel
 
 import qdarkstyle
+import urllib.request
 
 
 def colortext(color, text):
@@ -27,6 +29,21 @@ def styletext(stylecode, text):
     return styled_text
 
 
+def list_entry(json_data, title, tag):
+    entry = ["", ""]
+
+    entry[0] = title
+
+    answer = json_data.get(tag, "N/A")
+    #line_length = 50
+    #list_lines = [answer[i:i+line_length] for i in range(0, len(answer), line_length)]
+
+    # for line in list_lines:
+    #    entry[1] += line + "\n"
+
+    return answer
+
+
 class Widget(QWidget):
 
     def widget_by_name(self, name):
@@ -37,20 +54,67 @@ class Widget(QWidget):
         self.load_ui()
         self.setWindowTitle("ClipEZ-Next")
 
-    def click_download(self):
+        self.widget_by_name("button_cleartext").clicked.connect(self.clear_url)
+        self.widget_by_name("button_getinfo").clicked.connect(self.get_video_info)
+        self.widget_by_name("button_downloadstart").clicked.connect(self.click_download)
+        self.widget_by_name("lineedit_url").returnPressed.connect(self.click_download)
+        self.widget_by_name("lineedit_url").textChanged.connect(self.enable_clear_text_button)
+
+
+        self.widget_by_name("image_thumb").setMinimumWidth(self.widget_by_name("treeview_detailed").height()*9/16)
+        self.widget_by_name("image_thumb").setMaximumWidth(self.widget_by_name("treeview_detailed").height()*9/16)
+        self.widget_by_name("image_thumb").setFixedWidth(self.widget_by_name("treeview_detailed").height()*9/16)
+
+        self.enable_clear_text_button()
+
+    def get_video_info(self):
         self.widget_by_name("textbox_status").append(styletext("b", colortext("Green", "INFO: "))+"Getting Video Info...")
         ydl = yt_dlp.YoutubeDL({})
         url_text = self.widget_by_name("lineedit_url").text()
         try:
             info_dict = ydl.extract_info(url=url_text, download=False)
-            document = json.loads(json.dumps(info_dict, indent=4))
+            json_data = json.dumps(info_dict, indent=4)
+            print(info_dict)
+            document = json.loads(json_data)
             model = jsonmodel.JsonModel()
             model.load(document)
             self.widget_by_name("treeview_detailed").setModel(model)
             self.widget_by_name("textbox_status").append(styletext("b", colortext("Green", "INFO: ")) + "Got Video Info!")
+
+            list_info = [
+                ["title", info_dict.get("title", None)],
+                ["uploader", info_dict.get("uploader", None)],
+                ["id", info_dict.get("id", None)],
+                ["webpage_url", info_dict.get("webpage_url", None)],
+                ["thumbnail", info_dict.get("thumbnail", None)],
+                ["view_count", info_dict.get("view_count", None)],
+                ["duration", info_dict.get("duration", None)],
+                ["upload_date", info_dict.get("upload_date", None)],
+                ["age_limit", info_dict.get("age_limit", None)],
+                ["uploader_id", info_dict.get("uploader_id", None)],
+                ["uploader_url", info_dict.get("uploader_url", None)],
+                ["Subscribers", info_dict.get("channel_follower_count", None)],
+            ]
+
+            for i in range(len(list_info)):
+                self.widget_by_name("table_simple").insertRow(self.widget_by_name("table_simple").rowCount())
+                self.widget_by_name("table_simple").setItem(i, 0, QTableWidgetItem(list_info[i][0]))
+                self.widget_by_name("table_simple").setItem(i, 1, QTableWidgetItem(list_info[i][1]))
+
+            thumb_url = info_dict.get("thumbnail", None)
+            if thumb_url != None:
+                with urllib.request.urlopen(thumb_url) as _url:
+                    data = _url.read()
+                    px = QPixmap()
+                    px.loadFromData(data)
+                    self.widget_by_name("image_thumb").setPixmap(px)
+
         except Exception as e:
             print(e)
             self.widget_by_name("textbox_status").append(styletext("b", colortext("Red", "ERROR: ")) + "Unsupported URL: "+styletext("i", url_text))
+
+    def click_download(self):
+        self.get_video_info()
 
     def enable_clear_text_button(self):
         whether = (len(self.widget_by_name("lineedit_url").text()) != 0)
@@ -67,13 +131,6 @@ class Widget(QWidget):
         ui_file = QFile(path)
         ui_file.open(QFile.ReadOnly)
         loader.load(ui_file, self)
-
-        self.widget_by_name("button_cleartext").clicked.connect(self.clear_url)
-        self.widget_by_name("button_getinfo").clicked.connect(self.click_download)
-        self.widget_by_name("lineedit_url").returnPressed.connect(self.click_download)
-        self.widget_by_name("lineedit_url").textChanged.connect(self.enable_clear_text_button)
-
-        self.enable_clear_text_button()
 
         ui_file.close()
 
